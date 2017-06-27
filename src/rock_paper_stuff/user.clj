02881f1b1b3e9cbfa5dda7249cd0b0ea2335621a
Lee Spector <lspector@hampshire.edu>
2017-06-27 17:31:31 -0400

    Add random-pf and fix top-level call in user-main.

--- src/rock_paper_stuff/user.clj
+++ src/rock_paper_stuff/user.clj
@@ -4,13 +4,17 @@
 
 ;; Include your player functions and other definitions here.
 
+(defn random-pf
+  "A player function that returns a random kind of stuff to play."
+  [self other-skin]
+  {:play (rand-nth [:rock :paper :scissors :fire :water])})
+
 (defn user-main
   "This is the function that will be called when 'lein run' is run on the command line."
   []
   ;; Include your top-level call here.
-  ;; In this example we play a game between two randomly-playing players, printing
-  ;; trades as they happen, and return the summary of the game which will also be 
-  ;; printed.
-  (:summary (play-game [(player "Randy" random-pf)
-                        (player "Sandy" random-pf)]
-                       :print)))
+  ;;
+  ;; In this example we play a game between two randomly-playing players, and 
+  ;; pretty-print the summaries of all of the players at the end of the game.
+  (pprint (map summary (:players (play-game [(player "Randy" random-pf)
+                                             (player "Sandy" random-pf)])))))
